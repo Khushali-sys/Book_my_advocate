b5aebb476905405d175c1918c00ab4308eebf963
Khushali-sys <145544571+Khushali-sys@users.noreply.github.com>
2026-01-06 22:15:18 +0530

    Update urls.py

--- book_my_advocate/urls.py
+++ book_my_advocate/urls.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
@@ -13,7 +14,7 @@ urlpatterns = [
     path('payments/', include('payments.urls')),
     path('api/', include('api.urls')),
 ]
-
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
